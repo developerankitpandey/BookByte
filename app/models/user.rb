0510f8046a408078c4f624371af7e98e7285d331
Ankit Pandey <iamankitpandey801@gmail.com>
2024-03-19 18:07:38 +0530

    added google authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
     enum role: { buyer: 0, seller: 1}
         
@@ -20,6 +20,11 @@ class User < ApplicationRecord
     def become_seller!
       update(role: :seller)
     end 
+
+    def self.from_google(u)
+      create_with(uid: u[:uid], provider: 'google',
+                  password: Devise.friendly_token[0, 20]).find_or_create_by!(email: u[:email])
+    end
 
     private 
 
